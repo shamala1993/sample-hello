20bc0086673c3da84c7a4a420d2cce9333f1c90d
ramya <ramyashamala18@gmail.com>
2018-06-14 20:47:15 -0700

    updates

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
 def call(String ID = 'My_Credentials', String P = 'PASSWORD', String U = 'USER') {
-  withCredentials([usernamePassword(credentialsId: '${ID}', passwordVariable: '${P}', usernameVariable: '${U}')]) {
+  withCredentials([usernamePassword(credentialsId: "${ID}", passwordVariable: "${P}", usernameVariable: "${U}")]) {
 }
 }
 
